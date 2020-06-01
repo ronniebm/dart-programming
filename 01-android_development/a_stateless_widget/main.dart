a9d9af9cb1d6a5b56cc7bdcca07533e571bc2e54
ronniebm <ronnie.coding@gmail.com>
2020-05-31 22:50:28 -0500

    updating main.dart

--- 01-android_development/a_stateless_widget/main.dart
+++ 01-android_development/a_stateless_widget/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  /*This is the first run of the App*/
   runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false, //'Debug' banner dissabled.
     home: MyApp(),
   ));
 }
 
+/*My App Stateless Widget*/
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /*Default variables*/
+    final double iconSize = 50.0;
+    final TextStyle textStyle =
+        TextStyle(color: Colors.blueGrey, fontSize: 16.0);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Stateless Widget'),
@@ -16,18 +24,30 @@ class MyApp extends StatelessWidget {
       body: Container(
         child: Center(
             child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             MyCard(
-              title: Text('I Love flutter'),
-              icon: Icon(Icons.favorite),
+              title: Text(
+                'I Love dart + flutter',
+                style: textStyle,
+              ),
+              icon: Icon(Icons.favorite,
+                  size: iconSize, color: (Colors.redAccent)),
             ),
             MyCard(
-              title: Text('I Like this Video'),
-              icon: Icon(Icons.thumb_up),
+              title: Text(
+                'I Like this programming style',
+                style: textStyle,
+              ),
+              icon: Icon(Icons.thumb_up, size: iconSize, color: (Colors.cyan)),
             ),
             MyCard(
-              title: Text('Next Video !'),
-              icon: Icon(Icons.queue_play_next),
+              title: Text(
+                'Let\'s continue learning',
+                style: textStyle,
+              ),
+              icon: Icon(Icons.queue_play_next,
+                  size: iconSize, color: (Colors.lightBlue)),
             )
           ],
         )),
@@ -36,19 +56,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/*My customized Card, Stateless Widget type*/
 class MyCard extends StatelessWidget {
   final Widget title;
   final Widget icon;
 
+  /*MyCard constructor*/
   MyCard({this.title, this.icon});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.only(bottom: 1.0),
       child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(50.0),
           child: Column(
-        children: <Widget>[this.title, this.icon],
-      )),
+            children: <Widget>[this.title, this.icon],
+          ),
+        ),
+      ),
     );
   }
 }
